2b2a22c35b1568f61bbcbdcddcf587f352ed751e
Angelica Aguilar <isaj44631@gmail.com>
2025-11-30 17:10:36 -0600

    include/ Agrega functions.h

---
+++ include/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include "person.h"
+
+int compare_by_height(const void *a, const void *b);
+int compare_by_name(const void *a, const void *b);
+int compare_by_age(const void *a, const void *b);
+
+#endif
