adef26c64f5d55f94bd8d7ffac8ecf3ba4e91822
Angelica Aguilar <isaj44631@gmail.com>
2025-12-01 11:16:54 -0600

    Correccion en las funciones para que reciban valores en lugar de punteros

--- include/functions.h
+++ include/functions.h
@@ -7,7 +7,7 @@ int compare_by_name(const void *a, const void *b);
 int compare_by_age(const void *a, const void *b);
 
 int validate_name(const char *name);
-int validate_age(const char *age);
-int validate_height(const char *height);
+int validate_age(int age);
+int validate_height(double height);
 
 #endif
