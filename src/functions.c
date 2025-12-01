572f08615607d958eeb958ba023f3d11c7332f77
Angelica Aguilar <isaj44631@gmail.com>
2025-11-30 21:55:54 -0600

    src/ Agregar validacion de nombre

--- src/functions.c
+++ src/functions.c
@@ -22,3 +22,27 @@ int compare_by_age(const void *a, const void *b) {
 	return p1->age - p2->age;
 
 }
+
+int validate_name(const char *name) {
+	int len = strlen(name);
+
+	if (len == 0 || len > 50)
+		return 0;
+
+	int only_num = 1;
+
+	for (int i = 0; i < len; i++) { //pusimos que era de cincuenta lo maximo, aqui se hace comprobacion con el len
+		if (!(name[i] >= '0' && name[i] <= '9')) {
+			only_num = 0;
+			break;
+		}
+	}
+
+	if (only_num)
+		return 0;
+
+	return 1;
+
+}
+
+
