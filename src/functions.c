8eb168d72144e031715675d61c509569bdabe16a
Angelica Aguilar <isaj44631@gmail.com>
2025-11-30 22:07:58 -0600

    src/ Agregar validaciones de edad y altura

--- src/functions.c
+++ src/functions.c
@@ -26,12 +26,12 @@ int compare_by_age(const void *a, const void *b) {
 int validate_name(const char *name) {
 	int len = strlen(name);
 
-	if (len == 0 || len > 50)
+	if (len == 0 || len > 50) //pusimos que era de cincuenta lo maximo, aqui se hace comprobacion con el len
 		return 0;
 
 	int only_num = 1;
 
-	for (int i = 0; i < len; i++) { //pusimos que era de cincuenta lo maximo, aqui se hace comprobacion con el len
+	for (int i = 0; i < len; i++) { //aqui es que si only_num devuelve cero es porque si hay numeros de cero a nueve, eso es invalido porque no es un nombre
 		if (!(name[i] >= '0' && name[i] <= '9')) {
 			only_num = 0;
 			break;
@@ -45,4 +45,44 @@ int validate_name(const char *name) {
 
 }
 
+int validate_age(const char *age) {
+	
+	int len = strlen(age);
 
+
+	if (len == 0) return 0;
+	for (int i = 0; i < len; i++) {
+		if (!(age[i] >= '0' && age[i] <= '9')) { //mismo que en la validacion de nombre
+			return 0;
+		}
+	}
+
+	return 1;
+
+}
+
+int validate_height(const char *height) {
+
+	int len = strlen(height);
+	if (len == 0) return 0;
+
+	int decimal = 0;
+
+	for (int i = 0, i < len; i++) {
+		char c = heiht[i];
+
+		if (c == '.') {
+			decimal++; // aqui se aumenta decimal solo si hay un punto detras y por eso retorna cero
+			if (decimal > 1) return 0;
+
+		}
+
+		else if (!(c >= '0' && c <= '9')) { //misma comprobacion de age y name
+			return 0;
+		}
+
+	}
+
+	return 1;
+
+}
