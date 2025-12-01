ff44e45a37a877d2b6cdd11242c0fcedcea04d51
Angelica Aguilar <isaj44631@gmail.com>
2025-11-30 22:10:57 -0600

    include/ Declara las funciones de validaciones

--- include/functions.h
+++ include/functions.h
@@ -6,4 +6,8 @@ int compare_by_height(const void *a, const void *b);
 int compare_by_name(const void *a, const void *b);
 int compare_by_age(const void *a, const void *b);
 
+int validate_name(const char *name);
+int validate_age(const char *age);
+int validate_height(const chart *height);
+
 #endif
