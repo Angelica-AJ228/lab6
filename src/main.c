a70f40f4c42a3d260680651fd040cf253cb3a596
Angelica Aguilar <isaj44631@gmail.com>
2025-12-01 10:44:07 -0600

    src/ Correcciones en typos y faltaba un ;

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "../include/Person.h"
 #include "../include/functions.h"
@@ -19,37 +20,37 @@ int main() {
 	
 	for (int i = 0; i < n; i++) {
 		
-		if (!validate_name(names[i])) {
+		if (!validate_name(name[i])) {
 			printf("ERROR: nombre no válido\n");
 			return 1;
 		}
 		
-		if (!validate_age(ages[i])) {
+		if (!validate_age(age[i])) {
 			printf("ERROR: valor de edad no válido\n");
 			return 1;
 		}
 		
-		if (!validate_height(heights[i])) {
+		if (!validate_height(height[i])) {
 			printf("ERROR: valor de altura no válido\n");
 			return 1;
 		}
 		
-		strcpy(people[i].name, names[i]);
-		people[i].age = atoi(ages[i]); //atoi convierte texto a int pero solo si el texto se alinea con los caracteres numericos
-		people[i].height = atof(heights[i]); //casi que lo mismo que atoi pero con float, en altura estamos usando decimales
+		strcpy(people[i].name, name[i]);
+		people[i].age = atoi(age[i]); //atoi convierte texto a int pero solo si el texto se alinea con los caracteres numericos
+		people[i].height = atof(height[i]); //casi que lo mismo que atoi pero con float, en altura estamos usando decimales
 	}
 
 	printf("Arreglo original: \n");
 	print_people(people, n);
 
-	printf("Ordenados por: \n")
+	printf("Ordenados por: \n");
 
 	qsort(people, n, sizeof(Person), compare_by_name);
 	printf("Nombre\n");
 	print_people(people, n);
 
 	qsort(people, n, sizeof(Person), compare_by_age);
-        printf("Nombre\n");
+        printf("Edad\n");
         print_people(people, n);
 
         qsort(people, n, sizeof(Person), compare_by_height);
